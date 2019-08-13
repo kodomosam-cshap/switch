ea955237dd9ab7eee7e68469a630541465401e0a
kodom <sampaiorodrigomoreira@gmail.com>
2019-08-12 21:39:16 -0300

    exemplo

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha um tipo de transporte");
+
+            string escolha = Console.ReadLine();
+
+            switch (escolha)
+            {
+                case "carro":
+                    break;
+                case "onibus":
+                    break;
+                case "trem":
+                    break;
+                default:
+                    escolha = " indefinida";
+                break;                    
+
+            }
+
+            Console.WriteLine($"A sua escolha foi {escolha}");
+        }
+    }
+}
